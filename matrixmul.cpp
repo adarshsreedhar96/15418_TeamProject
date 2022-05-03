e48b78c856738a625c318c1e4789068e66039291
Bhakti Chaudhari <bhakti.chaudhari96@gmail.com>
2022-05-03 05:34:20 -0400

    matrix mul benchmark

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -35,18 +35,14 @@ public:
     {
         printf("workerThreadStart called by thread: %d\n", std::this_thread::get_id());
         matrixmul *args = static_cast<matrixmul *>(threadArgs);
-        printf("set by thread args are %d\n", args->mat1[0]);
         for (int i = 0; i < args->size; i++)
         {
             for (int j = 0; j < args->size; j++)
             {
                 int linear_idx = (args->size * (i)) + j;
-                int *res = args->output + linear_idx;
-                *res = 0;
                 for (int k = args->start_idx; k < args->end_idx; k++)
                 {
-                    // printf("thread args are %d,%d,%d\n", i, args->size * i + k, args->mat1[args->size * i + k]);
-                    *res += *(args->mat1 + (args->size * (i) + k)) * *(args->mat2 + (args->size * (k) + j));
+                    args->output[linear_idx] += *(args->mat1 + (args->size * (i) + k)) * *(args->mat2 + (args->size * (k) + j));
                 }
             }
         }
@@ -55,12 +51,9 @@ public:
     void getTasks(T **args, int numberOfTasks)
     {
         int *output = (int *)malloc(this->size * this->size * sizeof(int));
-        *args = (matrixmul *)malloc(sizeof(matrixmul *) * numberOfTasks);
-        // *args = static_cast<matrixmul *>(threadArgs);
         for (int i = 0; i < numberOfTasks; i++)
         {
-
-            // args[i] = (matrixmul *)malloc(sizeof(matrixmul));
+            args[i] = (matrixmul *)malloc(sizeof(matrixmul));
             args[i]->mat1 = this->mat1;
             args[i]->mat2 = this->mat2;
 
@@ -68,7 +61,6 @@ public:
             args[i]->output = output;
             args[i]->start_idx = i * (size / numberOfTasks);
             args[i]->end_idx = (i + 1) * (size / numberOfTasks);
-            printf("set by thread args are %d\n", this->mat1[2]);
         }
         args[numberOfTasks - 1]->end_idx = size;
     }
--- test_matrixmul.cpp
+++ test_matrixmul.cpp
@@ -13,10 +13,9 @@ int main()
 {
 
     const int numOfThreads = 2;
-    //
-    // void *args;
+    int numberOfTasks = numOfThreads;
     MatMul matMul;
-    MatMul::matrixmul *args = (MatMul::matrixmul *)malloc(sizeof(MatMul::matrixmul *) * numOfThreads);
+    MatMul::matrixmul **args;
     int mat1[16] = {
         1, 1, 1, 1,
         2, 2, 2, 2,
@@ -29,18 +28,19 @@ int main()
         3, 3, 3, 3,
         4, 4, 4, 4};
 
+    int size = 4;
+    args = (MatMul::matrixmul **)malloc(sizeof(MatMul::matrixmul *) * numberOfTasks);
     matMul.setInput(mat1, mat2, 4);
-    matMul.getTasks(&args, numOfThreads);
+    matMul.getTasks(args, numOfThreads);
     threadPool_PerThread threadPool(numOfThreads);
-    // threadPool.submit(&printEmpty, numOfThreads);
-    threadPool.submit(&MatMul::workerTask, &args, numOfThreads);
+    threadPool.submit(&MatMul::workerTask, args, numOfThreads);
     threadPool.dispatch();
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            int *temp = args[0].output + ((i - 1) * args[0].size) + j;
+            int *temp = args[0]->output + ((i)*args[0]->size) + j;
             printf("[%d][%d]: %d\n", i, j, *temp);
         }
     }
