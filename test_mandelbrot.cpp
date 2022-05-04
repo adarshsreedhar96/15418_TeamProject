e41fed8b74db2e64ab994ee01aaea97253ac9ee2
Adarsh Sreedhar <adarshsr@andrew.cmu.edu>
2022-05-04 01:31:55 -0400

    Adding timing logic

--- test_mandelbrot.cpp
+++ test_mandelbrot.cpp
@@ -18,8 +18,8 @@ int main()
     mandelbrotSerial(x0, y0, x1, y1, width, height, 0, height, maxIterations, output_serial);
 
 
-    const int numOfThreads = std::thread::hardware_concurrency();
-    int numberOfTasks = numOfThreads * 5;
+    const int numOfThreads = 2;//std::thread::hardware_concurrency();
+    int numberOfTasks = numOfThreads * 3;
     Mandelbrot mandelbrot;
 
     typedef Mandelbrot::WorkerArgs* WAPtr;
@@ -31,8 +31,11 @@ int main()
     
     threadPool threadPool(numOfThreads);
     threadPool.submit(&Mandelbrot::workerTask, args, numberOfTasks);
+    auto start_time = std::chrono::high_resolution_clock::now();
     threadPool.dispatch();
     threadPool.clearTasks();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    printf("time diff: %d\n", std::chrono::duration_cast<std::chrono::nanoseconds>(end_time-start_time).count());
 
     if (!verifyResult(output_serial, output_thread, width, height)) {
         printf("ERROR : Output from threads does not match serial output\n");
