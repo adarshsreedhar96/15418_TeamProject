16c4d29ffd61012ab37db7c652614422d38b6c7b
Bhakti Chaudhari <bhakti.chaudhari96@gmail.com>
2022-05-05 00:21:40 -0400

    add non-locking queue

--- threadpool_perthread.h
+++ threadpool_perthread.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <thread>
 #include <iostream>
-#include "queue.h"
+
 //#include "task.h"
 #include <atomic>
 #include <stdlib.h>
@@ -9,153 +9,197 @@
 
 #define STEALTASKS 0
 
-using namespace std;
-class threadPool_PerThread{
-    private:
+#if STEALTASKS
+#include "queue.h"
+#else
+#include "queue_nolock.h"
+#endif
 
-    public:
-        uint32_t num_of_threads;
-        std::atomic_bool runningFlag = ATOMIC_VAR_INIT(false);
-        std::atomic_bool breakFlag = ATOMIC_VAR_INIT(false);
-        std::atomic_bool isArgumentsPresent = ATOMIC_VAR_INIT(false);
-        std::vector<std::thread> myThreads;
-        std::vector<Queue> myQueues;
-        bool toSteal = false;
-        StealAmount stealAmount;
-        // constructor
-        threadPool_PerThread(int numOfThreads, bool toSteal, StealAmount stealAmount){
-            this->num_of_threads = numOfThreads;
-            this->toSteal = toSteal;
-            this->stealAmount = stealAmount;
-            // create an array of that many threads
-            for(int i=0;i<num_of_threads;i++){
-                // create a new queue instance
-                myQueues.push_back(Queue());
-            }
-            // create the pool
-            create_threads();
-            // not sure if this is necessary
-        }  
-        void create_threads(){
-            for(int i=0;i<num_of_threads;i++){
-                myThreads.push_back(std::thread(&threadPool_PerThread::worker, this, i));
-            }
+using namespace std;
+class threadPool_PerThread
+{
+private:
+public:
+    uint32_t num_of_threads;
+    std::atomic_bool runningFlag = ATOMIC_VAR_INIT(false);
+    std::atomic_bool breakFlag = ATOMIC_VAR_INIT(false);
+    std::atomic_bool isArgumentsPresent = ATOMIC_VAR_INIT(false);
+    std::vector<std::thread> myThreads;
+    std::vector<Queue> myQueues;
+    bool toSteal = false;
+    StealAmount stealAmount;
+    // constructor
+    threadPool_PerThread(int numOfThreads, bool toSteal, StealAmount stealAmount)
+    {
+        this->num_of_threads = numOfThreads;
+        this->toSteal = toSteal;
+        this->stealAmount = stealAmount;
+        // create an array of that many threads
+        for (int i = 0; i < num_of_threads; i++)
+        {
+            // create a new queue instance
+            myQueues.push_back(Queue());
         }
-        void worker(int index){
-            while(true){
-                if(runningFlag){
-                    if(isArgumentsPresent){
-                        // grab a task
-                        std::function<void(void*)> newTask;
-                        void* args;
-                        if (myQueues[index].pop_task(newTask, &args))
-                        {
-                            newTask(args);
-                        } else {
-                            // this means there are no entries in the queue.
-                            // Attempt to steal tasks
-                            if(toSteal && (!stealTasks(index)) && breakFlag) {
-                                    break;
-                            } else if(breakFlag) {
-                                    break;
-                            }
-                            //printf("size of queue for thread: %d is %d\n", index, myQueues[index].getSize(true));
-                        }
-                    } else {
-                        // grab a task
-                        std::function<void()> task;
-                        if (myQueues[index].pop_task(task))
-                        {
-                            task();
-                        } else {
-                            // this means there are no entries in the queue.
-                            // Attempt to steal tasks
-                            if(toSteal && (!stealTasks(index)) && breakFlag) {
-                                    break;
-                            } else if(breakFlag) {
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
+        // create the pool
+        create_threads();
+        // not sure if this is necessary
+    }
+    void create_threads()
+    {
+        for (int i = 0; i < num_of_threads; i++)
+        {
+            myThreads.push_back(std::thread(&threadPool_PerThread::worker, this, i));
         }
-        bool stealTasks(int index){
-            // iterate over the others in a ring manner
-            for(int i=index+1;i<num_of_threads;i++){
-                //printf("thread: %d has queue of size: %d\n", i, myQueues[i].getSize(true));
-                if(isArgumentsPresent){
-                    // create an array of Tasks
-                    std::vector<Task> stolenTasks;
-                    // grab a task
-                    //std::function<void(void*)> newTask;
-                    void* args;
-                    //if (myQueues[i].pop_task(newTask, &args)){
-                    //StealAmount stealAmount = STEALALLTASKS;
-                    if (myQueues[i].steal_task(&stolenTasks, stealAmount)){
-                        // we found a task! Lets steal it
-                        //printf("stealing task from thread: %d and giving to thread: %d\n", i, index);
-                        //printf("size of vector: %d\n", stolenTasks.size());
-                        //std::function<void(void*)> newTask = stolenTasks[0].task;
-                        //void* args = stolenTasks[0].args;
-                         while (!stolenTasks.empty())
-                        {
-                            Task poppedTask = stolenTasks.back();
-                            std::function<void(void*)> newTask = poppedTask.task;
-                            void* args = poppedTask.args;
-                            newTask(args);
-                            stolenTasks.pop_back();
-                        }
-                        //newTask(args);
-                        //return true;
-                        return false;
-                    } else {
-                        //printf("queue of thread: %d is empty\n", index);
-                    }
-                } else {
-                    std::function<void()> newTask;
-                    if (myQueues[i].pop_task(newTask)){
-                        // we found a task! Lets steal it
-                        //printf("stealing task from thread: %d and giving to thread: %d\n", i, index);
-                        myQueues[index].push_task(newTask);
-                        return true;
-                    } else {
-                        //printf("queue of thread: %d is empty\n", index);
-                    }
-                }
-            }
-            return false;
-        }
-        void destroy_threads(){
-            for (uint32_t i = 0; i < num_of_threads; i++)
+    }
+    void worker(int index)
+    {
+        while (true)
+        {
+            if (runningFlag)
             {
-                myThreads[i].join();
+                if (isArgumentsPresent)
+                {
+                    // grab a task
+                    std::function<void(void *)> newTask;
+                    void *args;
+                    if (myQueues[index].pop_task(newTask, &args))
+                    {
+                        newTask(args);
+                    }
+                    else
+                    {
+                        // this means there are no entries in the queue.
+                        // Attempt to steal tasks
+                        if (toSteal && (!stealTasks(index)) && breakFlag)
+                        {
+                            break;
+                        }
+                        else if (breakFlag)
+                        {
+                            break;
+                        }
+                        // printf("size of queue for thread: %d is %d\n", index, myQueues[index].getSize(true));
+                    }
+                }
+                else
+                {
+                    // grab a task
+                    std::function<void()> task;
+                    if (myQueues[index].pop_task(task))
+                    {
+                        task();
+                    }
+                    else
+                    {
+                        // this means there are no entries in the queue.
+                        // Attempt to steal tasks
+                        if (toSteal && (!stealTasks(index)) && breakFlag)
+                        {
+                            break;
+                        }
+                        else if (breakFlag)
+                        {
+                            break;
+                        }
+                    }
+                }
             }
         }
-        void submit(const std::function<void()> &task, int numberOfTasks){
-            isArgumentsPresent = false;
-            // put tasks onto each queue
-            // but how do we access them, and also get track of their threads?
-            for(int i=0;i<numberOfTasks;i++){
-                myQueues[i%num_of_threads].push_task(task);
+    }
+    bool stealTasks(int index)
+    {
+        // iterate over the others in a ring manner
+        for (int i = index + 1; i < num_of_threads; i++)
+        {
+            // printf("thread: %d has queue of size: %d\n", i, myQueues[i].getSize(true));
+            if (isArgumentsPresent)
+            {
+                // create an array of Tasks
+                std::vector<Task> stolenTasks;
+                // grab a task
+                // std::function<void(void*)> newTask;
+                void *args;
+                // if (myQueues[i].pop_task(newTask, &args)){
+                // StealAmount stealAmount = STEALALLTASKS;
+                if (myQueues[i].steal_task(&stolenTasks, stealAmount))
+                {
+                    // we found a task! Lets steal it
+                    // printf("stealing task from thread: %d and giving to thread: %d\n", i, index);
+                    // printf("size of vector: %d\n", stolenTasks.size());
+                    // std::function<void(void*)> newTask = stolenTasks[0].task;
+                    // void* args = stolenTasks[0].args;
+                    while (!stolenTasks.empty())
+                    {
+                        Task poppedTask = stolenTasks.back();
+                        std::function<void(void *)> newTask = poppedTask.task;
+                        void *args = poppedTask.args;
+                        newTask(args);
+                        stolenTasks.pop_back();
+                    }
+                    // newTask(args);
+                    // return true;
+                    return false;
+                }
+                else
+                {
+                    // printf("queue of thread: %d is empty\n", index);
+                }
+            }
+            else
+            {
+                std::function<void()> newTask;
+                if (myQueues[i].pop_task(newTask))
+                {
+                    // we found a task! Lets steal it
+                    // printf("stealing task from thread: %d and giving to thread: %d\n", i, index);
+                    myQueues[index].push_task(newTask);
+                    return true;
+                }
+                else
+                {
+                    // printf("queue of thread: %d is empty\n", index);
+                }
             }
         }
-        template <typename T>
-        void submit(const std::function<void(void*)> &task, T** args, int numberOfTasks){
-            isArgumentsPresent = true;
-            // how do we get to know the size?
-            // we split into num_of_threads for now
-            for(int i=0;i<numberOfTasks;i++) {
-                // since number of threads and number of tasks may not be the same
-                // need this
-                myQueues[i%num_of_threads].push_task(task, *(args+i));
-            }
+        return false;
+    }
+    void destroy_threads()
+    {
+        for (uint32_t i = 0; i < num_of_threads; i++)
+        {
+            myThreads[i].join();
         }
-        void dispatch(){
-            runningFlag = true;
+    }
+    void submit(const std::function<void()> &task, int numberOfTasks)
+    {
+        isArgumentsPresent = false;
+        // put tasks onto each queue
+        // but how do we access them, and also get track of their threads?
+        for (int i = 0; i < numberOfTasks; i++)
+        {
+            myQueues[i % num_of_threads].push_task(task);
         }
-        void clearTasks(){
-            breakFlag = true;
-            destroy_threads();
+    }
+    template <typename T>
+    void submit(const std::function<void(void *)> &task, T **args, int numberOfTasks)
+    {
+        isArgumentsPresent = true;
+        // how do we get to know the size?
+        // we split into num_of_threads for now
+        for (int i = 0; i < numberOfTasks; i++)
+        {
+            // since number of threads and number of tasks may not be the same
+            // need this
+            myQueues[i % num_of_threads].push_task(task, *(args + i));
         }
+    }
+    void dispatch()
+    {
+        runningFlag = true;
+    }
+    void clearTasks()
+    {
+        breakFlag = true;
+        destroy_threads();
+    }
 };
