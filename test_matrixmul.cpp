cf9c8de6509c50b7b3abb45cf56aa69c154cabf6
Bhakti Chaudhari <bhakti.chaudhari96@gmail.com>
2022-05-04 21:57:15 -0400

    improve benchmark

--- test_matrixmul.cpp
+++ test_matrixmul.cpp
@@ -14,50 +14,38 @@ int main()
 {
 
     const int numOfThreads = 8;
-    int numberOfTasks = 32;
+    int numberOfTasks = 100;
     MatMul matMul;
     MatMul::matrixmul **args;
-    int mat1[100] = {
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+    int size = 500;
+    int mat1[size * size];
+    int mat2[size * size];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            mat1[i * size + j] = i + j;
+            mat2[i * size + j] = i + j;
+        }
+    }
 
-    int mat2[100] = {
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
-
-    int size = 10;
     args = (MatMul::matrixmul **)malloc(sizeof(MatMul::matrixmul *) * numberOfTasks);
     matMul.setInput(mat1, mat2, size);
     matMul.getTasks(args, numberOfTasks);
     threadPool_PerThread threadPool(numOfThreads);
-    //int *priorities = (int *)malloc(sizeof(int) * numberOfTasks);
-    //threadPool.submit(&MatMul::workerTask, args, priorities, numberOfTasks);
+    // int *priorities = (int *)malloc(sizeof(int) * numberOfTasks);
+    // threadPool.submit(&MatMul::workerTask, args, priorities, numberOfTasks);
     threadPool.submit(&MatMul::workerTask, args, numberOfTasks);
     threadPool.dispatch();
     threadPool.clearTasks();
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < size; j++)
         {
             int *temp = args[0]->output + ((i)*args[0]->size) + j;
-            printf("[%d][%d]: %d\n", i, j, *temp);
+            printf("%d ", *temp);
         }
+        printf("\n");
     }
 }
